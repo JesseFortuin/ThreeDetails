9b991eaf6c7d213b52559055afed657c479d3f83
jesse <jessefortuin850@gmail.com>
2023-06-05 12:13:39 +0200

    Feat: Created test setup, README.md, .gitignore

---
+++ ThreeDetails.Test/ThreeDetailsCalculatorTest.cs
@@ -0,0 +1,24 @@
+namespace ThreeDetails.Test
+{
+    public class ThreeDetailsCalculatorTest
+    {
+        [Theory]
+        [InlineData(6, 2)]
+        [InlineData(7, 1)]
+        [InlineData(4, 0)]
+        [InlineData(10, 2)]
+        [InlineData(15, 1)]
+        [InlineData(3, 1)]
+        public void Test1(int input, int expected)
+        {
+            //arrange
+            IThreeDetails details = new ThreeDetails();
+
+            //act
+            int actual = details.ThreeDetails(input);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
