8d981360e1d6ff5c4e7cb001b9e180d65c15b03f
jesse <jessefortuin850@gmail.com>
2023-07-03 12:49:33 +0200

    Feat: added dictionary to skip repeated branches

--- ThreeDetails.BL/ThreeDetailsCalculator.cs
+++ ThreeDetails.BL/ThreeDetailsCalculator.cs
@@ -2,6 +2,8 @@
 {
     public class ThreeDetailsCalculator : IThreeDetailsCalculator
     {
+        Dictionary<int, int> repeatedSeries = new Dictionary<int, int>();
+
         public int ThreeDetails(int n)
         {
             if (n == 3)
@@ -9,17 +11,24 @@
                 return 1;
             }
             
-            if (n <= 2)
+            if (n <= 2 || n == 4)
             {
                 return 0;
+            }
+
+            if (repeatedSeries.ContainsKey(n))
+            {
+                return repeatedSeries[n];
             }
 
             var list = Enumerable.Range(1, n);
 
             var evenNumberCount = list.Where(x => x % 2 == 0).Count();
 
-            var oddNumberCount = list.Where(x => x % 2 != 0).Count();
+            var oddNumberCount = list.Count() - evenNumberCount;
 
+            repeatedSeries.Add(n, ThreeDetails(evenNumberCount) + ThreeDetails(oddNumberCount));
+            
             return ThreeDetails(evenNumberCount) + ThreeDetails(oddNumberCount);
         }
     }
